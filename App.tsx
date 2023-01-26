0265f7eebd5f86eeb8ff8e75a84281d6d7f45159
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-01-26 13:23:20 -0300

    a

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {
 } from "native-base";
 import { StatusBar } from "react-native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,7 +17,8 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <View /> : <View />}
+      <Loading />
+      {/* {fontsLoaded ? <View /> : <Loading />} */}
     </NativeBaseProvider>
   );
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,5 @@
+import { Center, Spinner } from "native-base";
+
+export function Loading(){
+    return <Center flex={1}><Spinner /></Center>
+}
