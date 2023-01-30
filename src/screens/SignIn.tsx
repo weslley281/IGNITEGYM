960b0a27e77697e29b461f907920b341987a0b75
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-01-30 17:00:38 -0300

    a

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,6 @@
 import { VStack, Image } from "native-base";
 
-import BackgroundImg from '@assets/background.png';
+import BackgroundImg from '../assets/background.png'
 
 export function SignIn() {
   return (
