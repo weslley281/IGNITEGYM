4e2f9b6bf5e527137848d0c6a23835bff87a2c80
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-02-03 17:31:46 -0300

    a

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,27 @@
-import { VStack, Image } from "native-base";
+import { VStack, Image, Text, Center, Heading } from 'native-base';
 
-import BackgroundImg from '../assets/background.png'
+import LogoSvg from '../assets/logo.svg';
+import BackgroundImg from '../assets/background.png';
 
 export function SignIn() {
   return (
     <VStack flex={1} bg="gray.700">
-      <Image 
+      <Image
         source={BackgroundImg}
         alt="Pessoas treinando"
         resizeMode="contain"
         position="absolute"
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine o seu corpo e sua mente
+        </Text>
+      </Center>
+
+      <Heading>Acesse sua conta</Heading>
     </VStack>
   );
 }
