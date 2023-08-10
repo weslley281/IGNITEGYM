f65a16754df9691f93d05a0af995b98cefc0eceb
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-08-09 21:13:26 -0400

    a

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -8,7 +8,7 @@ export function HomeHeader() {
     <HStack bg={'gray.600'} pt={16} px={8} alignItems={'center'}>
       <UserPhoto
         source={{
-          uri: 'https://scontent.fcgb10-1.fna.fbcdn.net/v/t39.30808-1/297150155_1651826215188392_4793578278034857764_n.jpg?stp=dst-jpg_p200x200&_nc_cat=104&cb=99be929b-59f725be&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeEZUJkqJCnvxgReQtgbUlSKRmCWTAbkYMpGYJZMBuRgylZ5GN7q8IMeGUQl_-MfqUrxry1ZRC4zOC69JF5tJFoR&_nc_ohc=ybrvYjnklEQAX-Bv7VW&_nc_ht=scontent.fcgb10-1.fna&oh=00_AfCBJ_brePRD3pKvlIpOxPbU-E_B-duv7LhB7952uSPUcA&oe=64D467A9',
+          uri: 'https://scontent.fcgb10-1.fna.fbcdn.net/v/t39.30808-1/336286057_1213537462700589_64666111073547397_n.jpg?stp=c0.0.200.200a_dst-jpg_p200x200&_nc_cat=107&cb=99be929b-59f725be&ccb=1-7&_nc_sid=2b6aad&_nc_eui2=AeFRqZ-STsQwEeAMP7Bgxs4sG6-9R0hJyF8br71HSEnIXzotbsQ8JgpxHL6zN4VxW15ZUMRVMSdlhOQrdiLhHAGd&_nc_ohc=LQX-af8cb9UAX8n_0JN&_nc_ht=scontent.fcgb10-1.fna&oh=00_AfC-J1cHg7vOFAYfWhC-rvsUULUTBbnU3NpI4MpYwaenBA&oe=64D92F13',
         }}
         size={16}
         alt="Imagem do usuário"
@@ -19,7 +19,7 @@ export function HomeHeader() {
         </Text>
 
         <Heading color={'gray.100'} fontSize={'md'}>
-          Anita Santana tu é gata
+          Weslley Ferraz
         </Heading>
       </VStack>
 
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,15 @@
+import { Button } from '@components/Button';
+import { Input } from '@components/Input';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
-import { Center, ScrollView, Skeleton, Text, VStack } from 'native-base';
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Skeleton,
+  Text,
+  VStack,
+} from 'native-base';
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 
@@ -25,7 +34,7 @@ export function Profile() {
           ) : (
             <UserPhoto
               source={{
-                uri: 'https://scontent.fcgb10-1.fna.fbcdn.net/v/t39.30808-1/297150155_1651826215188392_4793578278034857764_n.jpg?stp=dst-jpg_p200x200&_nc_cat=104&cb=99be929b-59f725be&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeEZUJkqJCnvxgReQtgbUlSKRmCWTAbkYMpGYJZMBuRgylZ5GN7q8IMeGUQl_-MfqUrxry1ZRC4zOC69JF5tJFoR&_nc_ohc=ybrvYjnklEQAX-Bv7VW&_nc_ht=scontent.fcgb10-1.fna&oh=00_AfCBJ_brePRD3pKvlIpOxPbU-E_B-duv7LhB7952uSPUcA&oe=64D467A9',
+                uri: 'https://scontent.fcgb10-1.fna.fbcdn.net/v/t39.30808-1/336286057_1213537462700589_64666111073547397_n.jpg?stp=c0.0.200.200a_dst-jpg_p200x200&_nc_cat=107&cb=99be929b-59f725be&ccb=1-7&_nc_sid=2b6aad&_nc_eui2=AeFRqZ-STsQwEeAMP7Bgxs4sG6-9R0hJyF8br71HSEnIXzotbsQ8JgpxHL6zN4VxW15ZUMRVMSdlhOQrdiLhHAGd&_nc_ohc=LQX-af8cb9UAX8n_0JN&_nc_ht=scontent.fcgb10-1.fna&oh=00_AfC-J1cHg7vOFAYfWhC-rvsUULUTBbnU3NpI4MpYwaenBA&oe=64D92F13',
               }}
               alt="Foto do usário"
               size={PHOTO_SIZE}
@@ -43,7 +52,30 @@ export function Profile() {
               Alterar Foto
             </Text>
           </TouchableOpacity>
+
+          <Input bg={'gray.600'} placeholder="Nome" />
+          <Input
+            bg={'gray.600'}
+            placeholder="weslleyhenrique800@gmail.com"
+            isDisabled
+          />
         </Center>
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color={'gray.200'} fontSize={'md'} mb={2}>
+            Alterar Senha
+          </Heading>
+
+          <Input bg={'gray.600'} placeholder="Senha antiga" secureTextEntry />
+
+          <Input bg={'gray.600'} placeholder="Nova senha" secureTextEntry />
+          <Input
+            bg={'gray.600'}
+            placeholder="Confirme a nova senha"
+            secureTextEntry
+          />
+
+          <Button title="Atualizar" mt={5} />
+        </VStack>
       </ScrollView>
     </VStack>
   );
