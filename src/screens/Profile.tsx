9d6dec27df0babe2867d2e80b3618b5fd2c0a0aa
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-11-04 05:51:03 -0400

    a

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,85 +1,118 @@
-import { Button } from '@components/Button';
-import { Input } from '@components/Input';
-import { ScreenHeader } from '@components/ScreenHeader';
-import { UserPhoto } from '@components/UserPhoto';
-import {
-  Center,
-  Heading,
-  ScrollView,
-  Skeleton,
-  Text,
-  VStack,
-} from 'native-base';
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
+import {
+  Center,
+  ScrollView,
+  VStack,
+  Skeleton,
+  Text,
+  Heading,
+} from 'native-base';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+
+import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 const PHOTO_SIZE = 33;
 
 export function Profile() {
-  const [phoIsLoading, setPhoIsLoading] = useState(false);
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    'https://github.com/rodrigorgtic.png'
+  );
+
+  async function handleUserPhotoSelected() {
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        );
+        console.log(photoInfo);
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
-          {phoIsLoading ? (
+          {photoIsLoading ? (
             <Skeleton
               w={PHOTO_SIZE}
               h={PHOTO_SIZE}
-              rounded={'full'}
-              startColor={'gray.400'}
-              endColor={'gray.300'}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
             />
           ) : (
             <UserPhoto
-              source={{
-                uri: 'https://scontent.fcgb10-1.fna.fbcdn.net/v/t39.30808-1/336286057_1213537462700589_64666111073547397_n.jpg?stp=c0.0.200.200a_dst-jpg_p200x200&_nc_cat=107&cb=99be929b-59f725be&ccb=1-7&_nc_sid=2b6aad&_nc_eui2=AeFRqZ-STsQwEeAMP7Bgxs4sG6-9R0hJyF8br71HSEnIXzotbsQ8JgpxHL6zN4VxW15ZUMRVMSdlhOQrdiLhHAGd&_nc_ohc=LQX-af8cb9UAX8n_0JN&_nc_ht=scontent.fcgb10-1.fna&oh=00_AfC-J1cHg7vOFAYfWhC-rvsUULUTBbnU3NpI4MpYwaenBA&oe=64D92F13',
-              }}
-              alt="Foto do usário"
+              source={{ uri: userPhoto }}
+              alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
           )}
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelected}>
             <Text
-              color={'green.500'}
-              fontWeight={'bold'}
-              fontSize={'md'}
-              mb={8}
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
               mt={2}
+              mb={8}
             >
               Alterar Foto
             </Text>
           </TouchableOpacity>
 
-          <Input bg={'gray.600'} placeholder="Nome" />
-          <Input
-            bg={'gray.600'}
-            placeholder="weslleyhenrique800@gmail.com"
-            isDisabled
-          />
+          <Input bg="gray.600" placeholder="Nome" />
+
+          <Input bg="gray.600" placeholder="E-mail" isDisabled />
 
           <Heading
-            color={'gray.200'}
-            fontSize={'md'}
+            color="gray.200"
+            fontSize="md"
             mb={2}
-            alignSelf={'flex-start'}
+            alignSelf="flex-start"
             mt={12}
           >
-            Alterar Senha
+            Alterar senha
           </Heading>
 
-          <Input bg={'gray.600'} placeholder="Senha antiga" secureTextEntry />
+          <Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
 
-          <Input bg={'gray.600'} placeholder="Nova senha" secureTextEntry />
+          <Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
+
           <Input
-            bg={'gray.600'}
+            bg="gray.600"
             placeholder="Confirme a nova senha"
             secureTextEntry
           />
 
-          <Button title="Atualizar" mt={5} />
+          <Button title="Atualizar" mt={4} />
         </Center>
       </ScrollView>
     </VStack>
